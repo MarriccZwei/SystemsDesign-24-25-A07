9bbbc3df0b1fc491452f574af9296af765a719df
MarriccZwei <marricc7@gmail.com>
2024-09-02 19:18:29 +0200

    Update data_script.py

--- data_script.py
+++ data_script.py
@@ -2,4 +2,5 @@ import pandas as pd
 #TODO create a file with this python code in the computer and put there the aircraft data and then write it below to import from excel
 similar_ac_data = pd.read_excel('')
 
-print(similar_ac_data)
+if __name__ == "__main__":
+    print(similar_ac_data)
