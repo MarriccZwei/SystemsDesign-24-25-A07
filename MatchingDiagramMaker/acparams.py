605bcf78aa2cae2337009e18901b24844456bbcd
plamenaDD <plamid04@gmail.com>
2024-09-04 15:04:47 +0200

    Landing Field Length Parameters

--- MatchingDiagramMaker/acparams.py
+++ MatchingDiagramMaker/acparams.py
@@ -6,22 +6,8 @@
 TMIN=0.7
 CLMAX = 1.2
 VSTALL = 5 # [m/s]
-<<<<<<< Updated upstream
-BYPASS = 10 # bypass ratio of the engines
-CD0 = 0.01
-ASPECT = 10
-TAPER = 0.4
-OSWALD = 0.98
-<<<<<<< HEAD
-=======
 CLMAX_LAND = 2.2 
 LFL = 1500 # [m]
-RHO_LAND = 
->>>>>>> Stashed changes
-=======
-K_T = 0.85
-TOFL = 2790  # [m]
-H_2 = 11  # [m]
-RHO_TAKEOFF = 1.225 # [kg/m^3]
-g = 9.81 # [m/s]
->>>>>>> c1964e97c21eff3d9c0a7ce569809c8f9e41d5b0
+RHO_LAND = 1.225 # [kg/m^3]
+BHETA = 0.9 # Landing mass fraction
+CLFL = 0.45 # CS-25 specification
