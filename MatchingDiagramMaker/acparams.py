949da30a6065d539520e43ae214a3fe8e3669769
plamenaDD <plamid04@gmail.com>
2024-09-04 14:59:58 +0200

    Landing Field Length Parameters

--- MatchingDiagramMaker/acparams.py
+++ MatchingDiagramMaker/acparams.py
@@ -6,8 +6,14 @@
 TMIN=0.7
 CLMAX = 1.2
 VSTALL = 5 # [m/s]
+<<<<<<< Updated upstream
 BYPASS = 10 # bypass ratio of the engines
 CD0 = 0.01
 ASPECT = 10
 TAPER = 0.4
 OSWALD = 0.98
+=======
+CLMAX_LAND = 2.2 
+LFL = 1500 # [m]
+RHO_LAND = 
+>>>>>>> Stashed changes
