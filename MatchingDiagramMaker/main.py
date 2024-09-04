bdd17128caca4496434f84e7a6ad40c440926ddd
Kaaiman <Kajbreeman@gmail.com>
2024-09-04 16:41:11 +0200

    Update main.py

--- MatchingDiagramMaker/main.py
+++ MatchingDiagramMaker/main.py
@@ -19,8 +19,8 @@ for constraint in constraints.constraints:
 
 #input refrence aircraft data loading points
 loadingPointsList = refAcData.generateLoadingPoints()
-for point in loadingPointsList:
+for i, point in enumerate(loadingPointsList):
     plt.plot(point[0], point[1], 'r+')
-    #plt.text(point[0], point[1], point[2])
+    plt.text(point[0] + 30, point[1] + 0.005, i+1) #Hard coded numbers are offset of labels.
 
 plt.show()
