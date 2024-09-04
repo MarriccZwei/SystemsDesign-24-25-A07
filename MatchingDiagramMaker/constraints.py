cc7aee6f6ea69ce193c16aa9ed7b2361dde7dde1
MarriccZwei <marricc7@gmail.com>
2024-09-04 15:18:36 +0200

    Update constraints.py

--- MatchingDiagramMaker/constraints.py
+++ MatchingDiagramMaker/constraints.py
@@ -5,20 +5,31 @@ import acparams
 constraints = []
 
 #min T/W constraint - used for UAVS which need a large TWR due to their highly suboptimal aerodynamics
-def TWconstraint(WSaxis):
+'''TestConstraint-Do not uncomment for offcial use'''
+'''def TWconstraint(WSaxis):
     return WSaxis, np.zeros(len(WSaxis))+acparams.TMIN
 
-constraints.append(TWconstraint)
+constraints.append(TWconstraint)'''
 
 def StallSpeedconstraint(WSaxis): #here we need to start using the adsee book xd, just to demo a v line now
     return np.zeros(len(WSaxis))+acparams.VSTALL**2*1.225/2*acparams.CLMAX, WSaxis
 
 constraints.append(StallSpeedconstraint)
 
-def TestLinFunConstraint(WSaxis):
+'''TestConstraint-Do not uncomment for offcial use'''
+'''def TestLinFunConstraint(WSaxis):
     return WSaxis, np.sqrt(WSaxis)/WSaxis
 
-constraints.append(TestLinFunConstraint)
+constraints.append(TestLinFunConstraint)'''
+
+'''Climb gradient calculations'''
+def climb_gradient_general(WSaxis, density, nEngines, massFraction, gradient, thrustLapse): #do not append this one directly to constraints!!!
+    situationFraction = nEngines*massFraction/thrustLapse/(nEngines-1)
+    gradientFraction = gradient*gradient*density/2/WSaxis/massFraction
+    innerSqrt = (acparams.CD0*np.pi*acparams.ASPECT*acparams.OSWALD)**0.5
+    freeTerm = 4*acparams.CD0/np.pi/acparams.ASPECT/acparams.OSWALD
+    return situationFraction*np.sqrt(gradientFraction*innerSqrt+freeTerm)
+
 
 if __name__ == "__main__":
-    print(TWconstraint(np.linspace(0, 10,11)))
+    pass
