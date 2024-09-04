5334e0e96c1c23e71c46ab58bee35c3195853f89
Kaaiman <Kajbreeman@gmail.com>
2024-09-04 14:10:28 +0200

    Update data_script.py

--- data_script.py
+++ data_script.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import os
 #TODO create a file with this python code in the computer and put there the aircraft data and then write it below to import from excel
-similar_ac_data = pd.read_excel('')
+
+aircraftDataExcelPath = os.path.join(os.getcwd(), 'aircraftReferenceData.xlsx')
+aircraftDataFrame = pd.read_excel(aircraftDataExcelPath)
 
 if __name__ == "__main__":
-    print(similar_ac_data)
+    display(aircraftDataFrame)
