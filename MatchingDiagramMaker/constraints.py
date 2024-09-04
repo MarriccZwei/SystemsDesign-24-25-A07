635232fcbc098233b80f2fbc12507477bfcabdca
MarriccZwei <marricc7@gmail.com>
2024-09-04 15:57:23 +0200

    check2

--- MatchingDiagramMaker/constraints.py
+++ MatchingDiagramMaker/constraints.py
@@ -36,9 +36,9 @@ def climb_gradient_general(WSaxis, density, nEngines, nEnginesInoper, massFracti
     return situationFraction*np.sqrt(gradientFraction*innerSqrt+freeTerm)
 
 def TakeOffFieldLength(WSaxis):
-    return np.zeros(len(WSaxis)) + (1.15*thrustLapse*np.sqrt(WSaxis/(acparams.TAKEOFF_LENGTH*acparams.K_T*acparams.RHO_LAND*acparams.g*np.pi*acparams.ASPECT*acparams.OSWALD)) + 44/acparams.TAKEOFF_LENGTH)
+    return WSaxis, (1.15*thrustLapse.thrustLapse(0)*np.sqrt(WSaxis/(acparams.TAKEOFF_LENGTH*acparams.K_T*acparams.RHO_LAND*acparams.g*np.pi*acparams.ASPECT*acparams.OSWALD)) + 44/acparams.TAKEOFF_LENGTH)
 
-constraints.append(TakeOffFieldLength())
+constraints.append(TakeOffFieldLength)
 
 def CruiseSpeedConstraint(WSaxis):
     crmf = acparams.BETA_CRUISE
