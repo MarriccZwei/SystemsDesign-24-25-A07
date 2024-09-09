469816490deec09828772502bc0cdcb7d3820566
Eris845 <jonathan.halter@outlook.com>
2024-09-09 16:31:46 +0200

    misc

--- MatchingDiagramMaker/main.py
+++ MatchingDiagramMaker/main.py
@@ -13,12 +13,7 @@ WSres = 1000
 WSaxis = np.linspace(0, WSmax, WSres+1)
 plt.axis((0, WSmax, 0, TWmax))
 
-#0 - stallspeed
-#1 - 5 - climb gradient
-#6,7 - TO/Landing distance
-#8 - cruise speed
-#9 - climb speed
-
+#Start of intersection calculator
 resFactor = 10 #do not change, might break
 intxInterval = np.linspace(1, WSmax+1, WSres*resFactor+1)#custom interval to avoid div by zero errors
 f = constraints.CruiseSpeedConstraint(intxInterval)
@@ -41,14 +36,17 @@ intx = j
 inty = g[1][intx]
 print(intx)
 print(inty)
-
+pointName = "W/S: " + str(intx) + ", T/W: " + str(inty)
 plt.plot(intx, inty, '+r')
-plt.text(intx + 30, inty + 0.005, )
+plt.text(intx + 30, inty + 0.005, pointName)
+#End of intersection calculator
 
 #generating constraints
+i=0
 for constraint in constraints.constraints: 
-    plt.plot(*constraint(WSaxis))
-
+    plt.plot(*constraint(WSaxis),label=constraints.constraintNames[i])
+    i=i+1
+plt.legend()
 #input refrence aircraft data loading points
 loadingPointsList = refAcData.generateLoadingPoints()
 for i, point in enumerate(loadingPointsList):
