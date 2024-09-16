42df138bc4be4ccbfc9e6ad56ab91a3dc3f21a8a
Eris845 <jonathan.halter@outlook.com>
2024-09-16 15:47:43 +0200

    Update rollRate.py

--- WingDesign/rollRate.py
+++ WingDesign/rollRate.py
@@ -1,23 +1,26 @@
 
-def tau(chordRatio):
+def tau(chordRatio): #this is an approximation of the chart in the slides; it is not perfectly accurate, especially for x<0.2
     x = chordRatio
     if x<0.05:
         return x*4
     elif x<0.4:
         return (x*4/3) + (2 * 0.2 / 3)
-    elif x<0.7:
+    elif x<0.4:
         return x - 0.2
-        
-def rollRate(b1, b2, chordRatio, span, moment, clalpha, deflection, diffRatio):
+    elif x<0.7:
+        return (2 * x / 3) + (10 / 3)
+    
+def rollRate(b1, b2, chordRatio, span, moment, clalpha, cd0, deflection = 10, diffRatio = 1):
+    if diffRatio != 1:
+        deflection = 0.5 * (1 + diffRatio)
+    
     span = 50 #TEMPORTARY
     moment = 10000 #TEMPORARY
     area = 100 #TEMPORARY
 
+    Clda = 2 * cl_alpha * tau(chordRatio) / (area * span) #TODO multiply by integral (b1,b2){ c(y) * y}dy
 
-    Clda = 2 * cl_alpha * tau / (area * span)
-
-    Clp = 
-
-    da = 
-    rate = -1 * Clda/Clp * da * 2 * moment / span
+    Clp = -4 * (clalpha + cd0) / (area * span * span) #TODO multiply by integral (b1,b2){ c(y) * y}dy
+    rate = -1 * Clda/Clp * deflection * 2 * moment / span
     print(":3")
+    return rate
