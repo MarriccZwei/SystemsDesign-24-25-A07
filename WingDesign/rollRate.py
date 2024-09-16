db9d5d0451e9f4df456ad7c8c4228e731f7b603b
Eris845 <jonathan.halter@outlook.com>
2024-09-16 15:11:39 +0200

    Added rollRate calc

---
+++ WingDesign/rollRate.py
@@ -0,0 +1,15 @@
+
+
+def rollRate(b1, b2, tau, span, moment, clalpha, deflection, ):
+    span = 50 #TEMPORTARY
+    moment = 10000 #TEMPORARY
+    area = 100 #TEMPORARY
+
+
+    Clda = 2 * cl_alpha * tau / (area * span)
+
+    Clp = 
+
+    da = 
+    rate = -1 * Clda/Clp * da * 2 * moment / span
+    print(":3")
