9677236040cc390c365113c599ddae542164e415
Eris845 <jonathan.halter@outlook.com>
2024-09-16 16:22:40 +0200

    Update rollRate.py

--- WingDesign/rollRate.py
+++ WingDesign/rollRate.py
@@ -1,3 +1,4 @@
+from MatchingDiagramMaker import acparams
 
 def tau(chordRatio): #this is an approximation of the chart in the slides; it is not perfectly accurate, especially for x<0.2
     x = chordRatio
@@ -9,18 +10,18 @@ def tau(chordRatio): #this is an approximation of the chart in the slides; it is
         return x - 0.2
     elif x<0.7:
         return (2 * x / 3) + (10 / 3)
-    
+
+#b1 and b2 are the spanwise locations of the start + end of the aileron
 def rollRate(b1, b2, chordRatio, span, moment, clalpha, cd0, deflection = 10, diffRatio = 1):
     if diffRatio != 1:
         deflection = 0.5 * (1 + diffRatio)
     
-    span = 50 #TEMPORTARY
-    moment = 10000 #TEMPORARY
-    area = 100 #TEMPORARY
+    span = 50 #TODO: change
+    moment = 10000 #TODO: change
+    area = 100#TODO: change
 
-    Clda = 2 * cl_alpha * tau(chordRatio) / (area * span) #TODO multiply by integral (b1,b2){ c(y) * y}dy
-
+    Clda = 2 * clalpha * tau(chordRatio) / (area * span) #TODO multiply by integral (b1,b2){ c(y) * y}dy
     Clp = -4 * (clalpha + cd0) / (area * span * span) #TODO multiply by integral (b1,b2){ c(y) * y}dy
     rate = -1 * Clda/Clp * deflection * 2 * moment / span
-    print(":3")
+
     return rate
