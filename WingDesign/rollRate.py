046e789112da3b104357dd946c508b2d5a1a2b5e
Eris845 <jonathan.halter@outlook.com>
2024-09-16 17:10:39 +0200

    misc

--- WingDesign/rollRate.py
+++ WingDesign/rollRate.py
@@ -1,4 +1,5 @@
-from MatchingDiagramMaker import acparams
+import json
+import math
 
 def tau(chordRatio): #this is an approximation of the chart in the slides; it is not perfectly accurate, especially for x<0.2
     x = chordRatio
@@ -12,13 +13,14 @@ def tau(chordRatio): #this is an approximation of the chart in the slides; it is
         return (2 * x / 3) + (10 / 3)
 
 #b1 and b2 are the spanwise locations of the start + end of the aileron
-def rollRate(b1, b2, chordRatio, span, moment, clalpha, cd0, deflection = 10, diffRatio = 1):
+def rollRate(b1, b2, chordRatio, moment, clalpha, cd0, deflection = 10, diffRatio = 1):
     if diffRatio != 1:
         deflection = 0.5 * (1 + diffRatio)
     
-    span = 50 #TODO: change
+    mainData = json.loads(open("Protocols/main.json"))
+    area = mainData["S"]
     moment = 10000 #TODO: change
-    area = 100#TODO: change
+    span = math.sqrt( mainData["S"] * mainData["AR"] )
 
     Clda = 2 * clalpha * tau(chordRatio) / (area * span) #TODO multiply by integral (b1,b2){ c(y) * y}dy
     Clp = -4 * (clalpha + cd0) / (area * span * span) #TODO multiply by integral (b1,b2){ c(y) * y}dy
