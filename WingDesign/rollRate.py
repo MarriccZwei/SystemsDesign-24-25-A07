f67000b9af68d5d2c2db5a12e2f50d67155e969d
Eris845 <jonathan.halter@outlook.com>
2024-09-16 15:33:37 +0200

    Update rollRate.py

--- WingDesign/rollRate.py
+++ WingDesign/rollRate.py
@@ -1,6 +1,14 @@
 
-
-def rollRate(b1, b2, tau, span, moment, clalpha, deflection, ):
+def tau(chordRatio):
+    x = chordRatio
+    if x<0.05:
+        return x*4
+    elif x<0.4:
+        return (x*4/3) + (2 * 0.2 / 3)
+    elif x<0.7:
+        return x - 0.2
+        
+def rollRate(b1, b2, chordRatio, span, moment, clalpha, deflection, diffRatio):
     span = 50 #TEMPORTARY
     moment = 10000 #TEMPORARY
     area = 100 #TEMPORARY
