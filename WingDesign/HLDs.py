5b169b2c1e4e1f11560c93fc13d9f418777166b8
Kaaiman <Kajbreeman@gmail.com>
2024-09-23 17:05:50 +0200

    Updated HLD

--- WingDesign/HLDs.py
+++ WingDesign/HLDs.py
@@ -1,28 +1,31 @@
 import numpy as np
-from math import tan, cos
+from math import tan, cos, radians, sqrt
 
-targetDeltaCL = 1.9
+targetDeltaCL = 0.9
 
 deltaFlap = 50
 flapFactor = 0.35
 
 surface = 409
-sweepLE = 0.5487971935252399
-sweepTE = 0.3
+sweepLE = radians(31)
+sweepTE = radians(19)
 
 cRoot = 8.12
 
 def deltaCl(delta, factor):
     dcCf = 0.01*delta + 0.4
+
     return 1.6 * (1+factor*dcCf)
 
 def flapSurface():
-    return targetDeltaCL*surface/(0.9*deltaCl(deltaFlap, flapFactor)*cos(sweepTE))
+    return ((targetDeltaCL*surface)/(0.9*deltaCl(deltaFlap, flapFactor)*cos(sweepTE)))
 
+print(flapSurface())
 a = tan(sweepTE)-0.5*tan(sweepTE)-0.5*tan(sweepLE)
-coeff = np.array([2*a, 2*cRoot, -1*flapSurface()])
-roots = np.polynomial.polynomial.polyroots(coeff)
-print(roots)
+b = cRoot
+c = -0.5*flapSurface()
+y = (-b+sqrt(b**2 -4*a*c))/(2*a)
+print(y)
 
 
 
