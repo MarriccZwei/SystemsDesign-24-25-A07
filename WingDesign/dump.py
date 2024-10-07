4c455bc9e94effcec7c77c1843ad095d1fe1dfa7
MarriccZwei <marricc7@gmail.com>
2024-10-07 15:09:13 +0200

    update clmax val

--- WingDesign/dump.py
+++ WingDesign/dump.py
@@ -9,27 +9,28 @@ import CLdesign
 import CruiseConditions
 
 with open(os.getcwd()+"/Protocols/main.json") as mainJson:
-    jsonDict = json.loads(''.join(mainJson.readlines()))
-    jsonDict["b"] = planform.b
-    jsonDict["tr"] = planform.TaperRatio
-    jsonDict["sweep"] = planform.QuarterSweep
-    jsonDict["MAC"]=planform.MACList[0]
-    jsonDict["XMAC"]=planform.MACList[1]
-    jsonDict["YLEMAC"]=planform.MACList[2]
-    jsonDict["sweepC/2"] = planform.SweepList[0]
-    jsonDict["sweepLE"]=planform.SweepList[1]
-    jsonDict["sweepTE"] = planform.SweepList[2]
-    jsonDict["dihedral"] = planform.dihedral
-    jsonDict["Cr"] = planform.CList[0]
-    jsonDict["Ct"] = planform.CList[1]
-    #jsonDict["bEndHLD"] = HLDs.y
-    jsonDict["CLmaxClean"] = stallConditions.maxCL(2.0, '64a210')
-    #jsonDict["CLmaxTO"] = stallConditions.maxCL(1.5, '64a210')[1]
-    #jsonDict["CLmaxLand"] = stallConditions.maxCL(1.5, '64a210')[2]
-    jsonDict["rollRate"] = rollRate.rollRate(7, 15, 0.25, 80, 10, 0.75)
-    jsonDict["CLDesign"] = CLdesign.cL
-    jsonDict["UltimateCL"] = 2.5
-    jsonDict["Incidence"] = CruiseConditions.alphaTrim(-1.25)
+    with open(os.getcwd()+"/Protocols/main.json") as mainJson:
+        jsonDict = json.loads(''.join(mainJson.readlines()))
+        jsonDict["b"] = planform.b
+        jsonDict["tr"] = planform.TaperRatio
+        jsonDict["sweep"] = planform.QuarterSweep
+        jsonDict["MAC"]=planform.MACList[0]
+        jsonDict["XMAC"]=planform.MACList[1]
+        jsonDict["YLEMAC"]=planform.MACList[2]
+        jsonDict["sweepC/2"] = planform.SweepList[0]
+        jsonDict["sweepLE"]=planform.SweepList[1]
+        jsonDict["sweepTE"] = planform.SweepList[2]
+        jsonDict["dihedral"] = planform.dihedral
+        jsonDict["Cr"] = planform.CList[0]
+        jsonDict["Ct"] = planform.CList[1]
+        #jsonDict["bEndHLD"] = HLDs.y
+        jsonDict["CLmaxClean"] = stallConditions.maxCL(1.69, '64a210')
+        #jsonDict["CLmaxTO"] = stallConditions.maxCL(1.5, '64a210')[1]
+        #jsonDict["CLmaxLand"] = stallConditions.maxCL(1.5, '64a210')[2]
+        jsonDict["rollRate"] = rollRate.rollRate(7, 15, 0.25, 80, 10, 0.75)
+        jsonDict["CLDesign"] = CLdesign.cL
+        jsonDict["UltimateCL"] = 2.5
+        jsonDict["Incidence"] = CruiseConditions.alphaTrim(-1.25)
 
 
 with open(os.getcwd()+"/Protocols/main.json", 'w') as mainJson:
