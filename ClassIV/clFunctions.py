ef9d32f82b71ec51dafa1830d342f817595e0b24
MarriccZwei <marricc7@gmail.com>
2024-10-15 12:29:44 +0200

    changes prior to changing kruger

--- ClassIV/clFunctions.py
+++ ClassIV/clFunctions.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 from math import sqrt, pi, tan
 from General import Constants as c
 from OOP.Planform import Planform
-from OOP.HLDs import HLDs
+#from OOP.HLDs import HLDs
 
 
 def clDesign(wingLoading, weightFuel, planform: Planform):
@@ -65,5 +65,5 @@ def maxCL(clmax2d, planform: Planform, mach = 0.0):
 
     return maxCLtrue
 
-def CLClean(planform: Planform, hld: HLDs, alpha, mach max=False):
-    dCda = dCLdAlpha(mach, planform)
+#def CLClean(planform: Planform, hld: HLDs, alpha, mach, max=False):
+    #dCda = dCLdAlpha(mach, planform)
