2d44366e4fdb71c9c413a63be2b297015a8ca004
Eris845 <jonathan.halter@outlook.com>
2024-10-16 17:02:53 +0200

    Code

--- ClassI/weightEstimation.py
+++ ClassI/weightEstimation.py
@@ -4,142 +4,42 @@ if __name__ == "__main__":
     import os
     sys.path.insert(1, os.getcwd())
 from math import sqrt, pi, exp, log
-import matplotlib.pyplot as plt
-import numpy as np
 from General import Constants
-import pitchUpConstraint
-import json
-import os
-inputData = json.load(open(os.getcwd()+"/ClassI/input.json"))
 
-#weights
-M_pl_max = Constants.MAXPAYLOAD#maximum payload
-M_pl_des = Constants.DESIGNPAYLOAD
-Mf_oe = inputData["MFoe"] #operational empty mass fraction (M_oe/M_mtow) avg of the similar aircraft TODO
+def TSFC(bypass):
+    return(22 * bypass**(-0.19))
 
-"""#variables used for C_d0 calculations, based on equation 6.15 from the adsee reader
-wet_wing_area = 6.2 #wetted area to wing area ratio found in the ADSEE book
-C_f = 0.0026 #skin friction coeff found in the ADSEE book
-"""
-#variables used to calculate oswald factor, based on equation 6.17
-eff_span = 0.97 #span efficiency found in the ADSEE book
-D_par = 0.0075 #lift dependent parasite drag found in the ADSEE book
- 
-AR = pitchUpConstraint.aspect(Constants.CRUISEMACH) #aspect ratio using the average of the reference aircraft
-B = inputData["BYPASS"]
+def engineEfficiency(TSFC):
+    Constants.CRUISEVELOCITY / (TSFC* Constants.SPECIFICENERGY)* (10**6)
 
-TSFC = 22*B**-0.19
+def Rlost(L_D):
+    return  (1/0.7) * L_D * (Constants.CRUISEALTITUDE + (Constants.CRUISEVELOCITY**2)/(2*Constants.G))
 
-e_spec = 43.5 *10**6 #specific energy for kerosene, value found in the ADSEE book
-g = Constants.G #gravitational acceleration constant
-h_cr = Constants.CRUISEALTITUDE #cruise altitude [m]
-v_cr = Constants.CRUISEVELOCITY #cruise speed [m/s]
-F_con = Constants.CONTINGENCYFUELFRACTION #fraction of fuel used for contingency 
-R_div = Constants.DIVERSIONRANGE * 1000 #diversion range of aircraft
-t_e = Constants.LOITERTIME * 60 #loiter time
-R_nom = Constants.DESIGNRANGE * 1000 #design mission range
+def Req(L_D):
+     R_nom  = Constants.DESIGNRANGE*1000
+     R_lost = Rlost(L_D)
+     F_con = Constants.CONTINGENCYFUELFRACTION
+     R_div = Constants
+     return ((R_nom + R_lost) * (1 + F_con) + 1.2 * R_div + Constants.CRUISEVELOCITY * Constants.LOITERTIME)
 
-eff_eng = v_cr / (TSFC* e_spec) #0.379 #engine efficiency based on equation 6.23 in the ADSEE book
-#print("FRAC:",TSFC* e_spec)
-Cd_0 = inputData["CD0"] #zero lift drag calculation
+def Raux(L_D):
+    return Req(L_D) - Constants.DESIGNRANGE
 
-e = 1/(pi * AR * D_par + (1/eff_span)) #oswald factor
+def MFfuel(L_D, TSFC):
+     return (1 - exp(-Req(L_D) / (engineEfficiency(TSFC) * (Constants.SPECIFICENERGY/ Constants.G) * L_D)))
 
-L_D = inputData["LIFTDRAGCRUISE"] #lift to drag ratio
+def mtom(MFoe, L_D, TSFC):
+    return (Constants.MAXPAYLOAD)/(1 - MFfuel(L_D, TSFC) - MFoe)
 
-#calculations of equivalent range
-R_lost = (1/0.7) * L_D * (h_cr + (v_cr**2)/(2*g)) #lost range 
-R_eq = ((R_nom + R_lost) * (1 + F_con) + 1.2 * R_div + v_cr * t_e)#equivalent range
-R_aux = R_eq - R_nom #auxilary range
+def Moe(MFoe, L_D, TSFC):
+    return MFoe * mtom(MFoe, L_D, TSFC)
 
-a1 = R_eq
-a2 = eff_eng * e_spec * L_D / g
-print(a1, "   ", a2)
+def Mfuel(MFoe, L_D, TSFC):
+    return mtom(MFoe, L_D, TSFC) - MFoe*mtom(MFoe, L_D, TSFC) - Constants.MAXPAYLOAD
 
-print("Fraction: ", (-R_eq / (eff_eng * (e_spec / g) * L_D)))
+def Rferry(MFoe, L_D, TSFC):
+    return (engineEfficiency(TSFC) * L_D * (Constants.SPECIFICENERGY/ Constants.G) * log((Moe(MFoe, L_D, TSFC) + Mfuel(MFoe, L_D, TSFC))/(Moe(MFoe, L_D, TSFC))) - Raux(L_D))
 
-print("Req: ", R_eq)
-print("Eff-eng: ", eff_eng)
-print("Espec: ", e_spec)
-print("L/D: ", L_D)
-print("G: ", g)
+def Rharm(MFoe, L_D, TSFC):
+    return (engineEfficiency(TSFC) * L_D * (Constants.SPECIFICENERGY/ Constants.G) * log(Constants.MAXPAYLOAD + (Moe(MFoe, L_D, TSFC) + Mfuel(MFoe, L_D, TSFC))/(Moe(MFoe, L_D, TSFC))) - Raux(L_D))
 
-Mf_ec = 1 - exp(-R_eq / (eff_eng * (e_spec / g) * L_D)) #fuel mass fraction
-
-
-M_mto = (M_pl_max)/(1 - Mf_ec - Mf_oe) #maximum take-off mass
-M_ec = Mf_ec * M_mto #fuel mass
-
-print("fuel and oew fractions:", Mf_ec, Mf_oe)
-
-M_oe = Mf_oe * M_mto #operating empty mass
-
-M_f = M_mto - M_oe - M_pl_max 
-
-
-print(M_oe, M_pl_max)
-
-R_ferry = eff_eng * L_D * (e_spec / g) * log((M_oe + M_ec)/(M_oe)) - R_aux  #ferry range
-R_harm = eff_eng * L_D * (e_spec / g) * log((M_oe + M_pl_max + M_f)/(M_oe + M_pl_max)) - R_aux #harmonic range
-
-print(M_mto)
-
-print(R_nom)
-print(R_harm)
-print(R_ferry)
-
-#plot
-ds = 50000 #step size [m]
-M_pl_list = [] #naming of Mass payload list
-
-#curve_one_der = (M_pl_max - M_pl_des)/(R_nom - R_harm) #kg / 500 km
-curve_two_der = (M_pl_des)/(R_ferry - R_nom) #kg / 500 km
-M_pl = M_pl_max #placeholder for the payload mass to append to the lists
-R_list = [] #np.arange(0,18000000,ds)
-R = 0 #placeholder for the range to append to the lists
-
-
-#generates the payload mass for the R_0 part of the curve
-for i in range(0, int(R_harm), ds):
-    M_pl_list.append(M_pl) 
-    R += ds
-    R_list.append(R/1000)
-
-print("maximum payload [kg]:")
-print(M_pl_max)
-print()
-print("Harmonic range [km]:")
-print(R_harm/1000)
-print()
-print("design payload [kg]:")
-print(M_pl_des)
-print()
-print("nominal range [km]:")
-print(R_nom/1000)
-print()
-print("ferry mass [kg]:")
-print(M_mto - M_pl_des)
-print()
-print("ferry range [km]:")
-print(R_ferry/1000)
-print()
-print("OEM [kg]:")
-print(M_oe)
-print()
-print("Fuel mass [kg]:")
-print(M_f)
-print()
-print("maximum take-off weight [kg]:")
-print(M_mto)
-print()
-
-plt.plot(R_list, M_pl_list)
-plt.yticks(fontsize=15)
-plt.xticks(fontsize=15)
-plt.xlabel("range [km]", fontsize=18)
-plt.ylabel("payload [kg]", fontsize=18)
-plt.title("payload range diagram", fontsize=25)
-plt.grid(visible=True, which="major", axis="both")
-plt.xlim(0)
-plt.ylim(0)
-plt.show()
