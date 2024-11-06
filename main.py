ecd6896e3919a3194a1aaf190e3ffdf323c07b4c
MarriccZwei <marricc7@gmail.com>
2024-11-06 22:40:47 +0100

    encapsulating the creation of matching diagram constraints into a single function

--- main.py
+++ main.py
@@ -6,3 +6,29 @@ import ClassIV.clFunctions as clFuns
 import ClassII.weightEst as wEstII
 import ClassII.LoadFactor as loadF
 import ClassII.dragEst as dragEst
+
+'''obtaining Initial values from main.json'''
+
+'''Iteration loop'''
+
+'''Class I weight est. Based on mass fractions and Cd/AR values from class II'''
+
+'''Matching Diagram. Figuring out a design point'''
+
+'''Wing Planform Design'''
+
+'''Aileron design'''
+
+'''HLD Design'''
+
+'''Fuselage & fuel Volume Calculations'''
+
+'''Tail and Cg and lg subloop'''
+
+'''Class II Drag'''
+
+'''Class II weight'''
+
+'''Repeat the Iteration loop until your class I and class II estimations converge'''
+
+'''SAR Calculation'''
