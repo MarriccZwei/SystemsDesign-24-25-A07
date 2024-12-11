bc882e6ec28eca4a369d8280103911b5dabdad64
MarriccZwei <marricc7@gmail.com>
2024-12-11 17:07:01 +0100

    Update Cell.py

---
+++ OOP/Cell.py
@@ -0,0 +1,7 @@
+import OOP.Planform as pf
+
+class Cell:
+    '''The representation of the wingbox between 2 ribs'''
+    #startPos, endPos - spanwise location [m] from root
+    def __init__(planform:pf.Planform, startPos:float, endPos:float, sst:float, sat:float, ssb:float, sab:float):
+        pass
