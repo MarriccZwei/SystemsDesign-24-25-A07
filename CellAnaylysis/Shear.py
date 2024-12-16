b7a14d87123a9fc765d1acb64044761253fbc6f7
gungorman <gungoremre2004@gmail.com>
2024-12-16 15:16:01 +0100

    Update Shear.py

--- CellAnaylysis/Shear.py
+++ CellAnaylysis/Shear.py
@@ -3,3 +3,12 @@ import numpy as np
 def crit_shear_tress(t, b, k_s, v, E):
     tau_crit = ((np.pi**2*k_s*E)*(t/b)**2/(12*(1-v**2)))
     return tau_crit
+
+def ideal_term():
+    
+
+
+def shearcalc_mid():
+    
+
+def shearcalc_nomid():
