0efb7b608f51803630ff5565e7091847a8da672f
mcruz05 <mdegouveapinto@tudelft.nl>
2024-12-16 16:27:50 +0100

    Update ColumnBuckling.py

--- CellAnaylysis/ColumnBuckling.py
+++ CellAnaylysis/ColumnBuckling.py
@@ -4,12 +4,17 @@ import General.Constants as consts
 
 def crit_buckling_stress (cell:cell.Cell, tip = False):
 
+    #  Choosing the constant K depending on the cell we are evaluating
     if tip:
         K = 0.25
     else:
         K = 4.
 
-    sigma_buckling = K * np.pi**2 * consts.E_MODULUS * cell.sectionProperties[ixx]
-    #TODO Find out how to put I in here, how to handle constant K and finish the formula
+    #  Area of the stringer
+    Area = cell.stringerDesign["t"]*cell.stringerDesign["w"] + cell.stringerDesign["t"]*cell.stringerDesign["h"] - (cell.stringerDesign["t"])**2
+
+    #  Column Buckiling Formula
+    sigma_buckling = (K * np.pi**2 * consts.E_MODULUS * cell.sectionProperties["ixx"]) / ((cell.zLen)**2 * Area) 
+
     return(sigma_buckling)
     
