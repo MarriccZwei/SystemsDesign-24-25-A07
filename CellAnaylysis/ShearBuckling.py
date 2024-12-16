29df28f4ba76c85334e31335e98117efe72e5d7b
luuk <97060135+luukmvb@users.noreply.github.com>
2024-12-16 16:41:27 +0100

    Update ShearBuckling.py

--- CellAnaylysis/ShearBuckling.py
+++ CellAnaylysis/ShearBuckling.py
@@ -1,4 +1,17 @@
+if __name__ == "__main__":
+    # ONLY FOR TESTING
+    import sys
+    import os
+    sys.path.insert(1, os.getcwd())
+    # ONLY FOR TESTING
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from OOP import Cell
+from OOP import FlexBox
+
+import Loads.WingSBT as SBT
+
 
 #cnsts
 v = 0.33
@@ -16,6 +29,9 @@ print(crit_shear_stress(4, 150, 10, 0.33, 72.4e9))
 
 
 def max_shear_stress(V, A, k_v):
+    V = SBT.combined_shear_load()
     tau_avg_shear = V/sum(A)
     tau_max_shear = k_v * tau_avg_shear
-    return tau_max_shear
+    return tau_max_shear\
+    
+# print(SBT.combined_shear_load())
