6fc5cc274f5df2bcbb654a15806565da73da8416
MarriccZwei <marricc7@gmail.com>
2024-12-18 16:04:00 +0100

    refactoring to cellOperations

--- CellAnaylysis/cellOperations.py
+++ CellAnaylysis/cellOperations.py
@@ -3,13 +3,13 @@ if __name__ == "__main__":
     import sys
     import os
     sys.path.insert(1, os.getcwd())
-    import matplotlib.pyplot as plt
     # ONLY FOR TESTING
 import OOP.Cell as cell
 from typing import Iterable, List
 import OOP.Planform as pf
 import numpy as np
 from General import Constants as c
+import matplotlib.pyplot as plt
 
 def cell_distr(planform, ribposes, stringerDesign, wingBoxThicknesses, cutoffidx, midSpar):
     '''A function that creates a distribution of the cells'''
@@ -19,6 +19,9 @@ def cell_distr(planform, ribposes, stringerDesign, wingBoxThicknesses, cutoffidx
             midSpar = None
         cells.append(cell.Cell(planform, ribposes[i-1], ribposes[i], stringerDesign, wingBoxThicknesses, midSpar))
     return cells
+
+def mofs(cells:List[cell.Cell], plot=False):
+    '''A function that conducts the analysis of each of the 6 failure modes'''
 
 if __name__ == "__main__":
     planform =pf.Planform(251.3429147793505, 9.872642920666417, 0.1, 28.503510117080133, 2.1496489882919865, False)
@@ -32,3 +35,7 @@ if __name__ == "__main__":
     distr = cell_distr(planform, ribposes, {'w':0.05, 'h':0.05, 't':0.005, 'st':0.1, 'sb':0.13}, {'f':0.004, 'b':0.012, 'r':0.004, 't':0.012, 'm':0.004}, 10, 0.4)
     for celli in distr:
         print(f"cell start: {celli.startPos}, cell end: {celli.endPos}")
+    poses = [celli.startPos for celli in distr]
+    ixxs = [celli.sectionProperties(0)["ixx"] for celli in distr]
+    plt.plot(poses, ixxs)
+    plt.show()
