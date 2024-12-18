2e70e65461b0dd2be3bbddf04e0f72d77657458b
luuk <97060135+luukmvb@users.noreply.github.com>
2024-12-18 14:38:20 +0100

    Update ShearBuckling.py

--- CellAnaylysis/ShearBuckling.py
+++ CellAnaylysis/ShearBuckling.py
@@ -21,21 +21,23 @@ if test == True:
 #cnsts
 v = 0.33 #poisson ratio
 E = 72.4e9 #young modulus
-k_s = 10 #assumption for now
+k_s = 10 #assumption for now TODO input it
 
 
 def crit_shear_stress(k_s):
     v = 0.33 #poisson ratio
     E = 72.4e9 #young modulus
-    t = FlexBox.wingboxThicknesses() #thickness of the web
-    b = FlexBox.length('f') #highest b gives lowest tau_critical, so the front spar 'f'
-    tau_crit = ((np.pi**2*k_s*E)*(t/b)**2/(12*(1-v**2)))
+    tau_crit = []
+    webs = ['f', 'm', 'r']
+    for i in webs:
+        t = FlexBox.thicknesses(i) #thickness of the web [m]
+        b = FlexBox.length(i) #highest b gives lowest tau_critical, so the front spar 'f' [m]
+        tau_crit = ((np.pi**2*k_s*E)*(t/b)**2/(12*(1-v**2)))
     return tau_crit
 
 
 #formula test
 # print(crit_shear_stress(4, 150, 10, 0.33, 72.4e9))
-
 
 
 def max_shear_stress(V, A):
