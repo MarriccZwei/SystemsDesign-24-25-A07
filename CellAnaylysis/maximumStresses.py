1f0910a9425e074ed85d0d356070b997542bdc45
mcruz05 <mdegouveapinto@tudelft.nl>
2025-01-03 18:28:34 +0100

    a

---
+++ CellAnaylysis/maximumStresses.py
@@ -0,0 +1,6 @@
+def MaxAxialStress(MomentX:float, MOIxx:float, MOIyy:float, MOIxy:float, FrontSparLen:float, CentroidX:float, Chord:float): 
+    Y = FrontSparLen/2 #location of maximum axial stress will likely be at the front spar
+    X = CentroidX*Chord
+    Denominator = MOIxx*MOIyy - MOIxy**2
+    MaxStress = MomentX*((MOIyy*Y - MOIxy*X)/(Denominator))
+
